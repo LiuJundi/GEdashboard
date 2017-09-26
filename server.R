cdf6beb4b754c95f7e41a33e2b6c12f55767c8fa
feardameow <shwang1121@gmail.com>
2017-09-25 17:31:50 -0700

    rearranged the columns to display better in table

--- server.R
+++ server.R
@@ -8,8 +8,10 @@ library(DT)
 
 load("GE Dashboard.RData")
 
-colnames(df.ge) <- c("Purchasing Document", "Vendor", "Material Number", "Plnt", "ABC Indicator", "Quantity", "Pdt",
-                     "PO Cycle Time", "Purchase Order Date", "Open PO")
+colnames(df.ge) <- c("Purchasing Document", "Vendor", "Material Number", "Plant ID", "ABC Indicator", "Quantity", "Planned Delivery Time",
+                     "Actual Delivery Time", "Purchase Order Date", "Open PO", "Predicted Delivery Date")
+
+df.ge <- df.ge[,c(1,2,3,4,5,9,11,7,8,10)]
 
 server <- function(input, output, session) {
   output$OpenOrdersTable = DT::renderDataTable({
