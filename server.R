091df9e6ebab5c69bf7a8e607e0a02bc3fb79940
feardameow <shwang1121@gmail.com>
2017-09-25 19:09:28 -0700

    fixed push changes

--- server.R
+++ server.R
@@ -7,29 +7,18 @@ library(dplyr)
 library(DT)
 load("GE Dashboard.RData")
 
+#Change format to date
 df.ge$Purchase.Order.Date <- as.Date(df.ge$Purchase.Order.Date, format="%b %d, %Y")
+
+#data frame for the bar chart
 ge.bar <- as.data.frame(table(df.ge$Purchase.Order.Date))
 colnames(ge.bar) <- c("OrderDate", "NumberofOrders")
 ge.bar$OrderDate <- format(as.Date(ge.bar$OrderDate), "%b%d")
 
-colnames(df.ge) <- c("Purchasing Document", "Vendor", "Material Number", "Plnt", "ABC Indicator", "Quantity", "Pdt",
-                     "PO Cycle Time", "Purchase Order Date", "Open PO")
-
-load("GE Dashboard.RData")
-
+#Naming and reordering columns for the table
 colnames(df.ge) <- c("Purchasing Document", "Vendor", "Material Number", "Plant ID", "ABC Indicator", "Quantity", "Planned Delivery Time",
                      "Actual Delivery Time", "Purchase Order Date", "Open PO", "Predicted Delivery Date")
-
 df.ge <- df.ge[,c(1,2,3,4,5,9,11,7,8,10)]
-
-
-load("GE Dashboard.RData")
-
-colnames(df.ge) <- c("Purchasing Document", "Vendor", "Material Number", "Plant ID", "ABC Indicator", "Quantity", "Planned Delivery Time",
-                     "Actual Delivery Time", "Purchase Order Date", "Open PO", "Predicted Delivery Date")
-
-df.ge <- df.ge[,c(1,2,3,4,5,9,11,7,8,10)]
-
 
 
 server <- function(input, output, session) {
