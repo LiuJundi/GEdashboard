4510ffe8b2038df2c9cbc0b7d35662752128df83
JundiLiu <jundiliu@uw.edu>
2017-09-27 10:55:39 -0700

    1.6v

--- server.R
+++ server.R
@@ -23,7 +23,7 @@ ge.pdd.bar$PredictedDate <- as.Date(ge.pdd.bar$PredictedDate)
 #Naming and reordering columns for the table
 colnames(df.ge) <- c("Purchasing Document", "Vendor", "Material Number", "Plant ID", "ABC Indicator", "Quantity", "Planned Delivery Time",
                      "Actual Delivery Time", "Purchase Order Date", "Open PO", "Predicted Delivery Date")
-df.ge <- df.ge[,c(1,2,3,4,5,9,11,7,8,10)]
+df.ge <- df.ge[,c(1,2,3,4,5,6,9,11,7,8,10)]
 
 
 server <- function(input, output, session) {
@@ -107,4 +107,40 @@ server <- function(input, output, session) {
     }
   })
   
+  #Prediction tab
+  observeEvent(input$predictButton, {
+    df.temp <- df.ge[0,c(4,5,6,9)] #plant, abc, quantity, pdt
+    df.temp[1,1] <- 3267
+    df.temp[1,2] <- df.ge$`ABC Indicator`[df.ge$`Material Number`==input$predictMaterial][1]
+    df.temp[1,3] <- input$predictQuantity
+    df.temp[1,4] <- input$predictPdt
+    colnames(df.temp) <- c("Plnt", "ABC.Indicator", "Quantity", "Pdt")
+    
+    pred.temp <- predict(rf.quantReg,newdata = df.temp[,c(2,3,4)],what=c(0.025,0.975))
+    
+    upperPred <- pred.temp[2]
+    middlePred <- predict(rf.reg, newdata = df.temp)
+    lowerPred <- pred.temp[1]
+    
+    output$lowerPrediction <- renderValueBox({
+      valueBox(
+        paste0(lowerPred), "Lower Prediction Interval", icon = icon("list"),
+        color = "purple"
+      )
+    })
+    
+    output$actualPrediction <- renderValueBox({
+      valueBox(
+        paste0(middlePred), "Predicted Delivery Days", icon = icon("list"),
+        color = "purple"
+      )
+    })
+    
+    output$upperPrediction <- renderValueBox({
+      valueBox(
+        paste0(upperPred), "Upper Prediction Interval", icon = icon("list"),
+        color = "purple"
+      )
+    })
+  })
 }
